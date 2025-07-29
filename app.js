d0f54e76427adc354d25fc946ddb0d14afc4fb44
faateh029 <buttfatih691@gmail.com>
2025-07-29 18:53:49 +0500

    setup basic server and envoirment

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 import express from 'express';
+import ejs from 'ejs'
+import { fileURLToPath } from 'url';
+import path from 'path';
+const PORT = process.env.PORT||4200;
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('view engine' , 'ejs');
+app.set('views' , path.join(__dirname , 'views'));
+
+app.get('/' , (req , res)=>{
+    console.log("get request");
+    res.status(200).json({msg:"get request running"});
+})
+
+app.listen(PORT , (req,res)=>{
+    console.log(`server running on port ${PORT}`)
+})
