24356d51d284f0152846fe9db2ba794acead4164
faateh029 <buttfatih691@gmail.com>
2025-07-30 13:48:00 +0500

    implemented get_categories_controller

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,4 +1,18 @@
+import {pool} from '../db/pool.js';
 export const get_categories_controller = async (req,res)=>{
+    const page = parseInt(req.query.page )||1;
+    const limit = parseInt(req.query.limit) ||5;
+    const offset = (page-1)*limit ; 
+    const totalCount = await pool.query(`SELECT COUNT(*) FROM categories`);
+    const paginatedData = await pool.query(`SELECT * FROM categories ORDER BY category_id  LIMIT $1 OFFSET $2` , [limit , offset])
+    const result = {
+        page:page,
+        limit:limit,
+        total:totalCount.rows[0].count,
+        totalPages: Math.ceil(Number(totalCount.rows[0].count/limit)),
+        data:paginatedData.rows
+    }
+    res.status(200).json(result);
 
 } 
 
