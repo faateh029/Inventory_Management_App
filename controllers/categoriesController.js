dafa53ca83b565da44e23e7944f382a2023ef208
faateh029 <buttfatih691@gmail.com>
2025-07-30 15:28:45 +0500

    setup list_categories for future use

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,5 +1,10 @@
 import {pool} from '../db/pool.js';
 export const get_categories_controller = async (req,res)=>{
+//     const items_count = await pool.query(`SELECT c.category_id, c.category_name, COUNT(i.item_id) AS item_count
+// FROM categories c
+// LEFT JOIN items i ON c.category_id = i.category_id
+// GROUP BY c.category_id
+// ORDER BY c.category_id;`)
     const page = parseInt(req.query.page )||1;
     const limit = parseInt(req.query.limit) ||5;
     const offset = (page-1)*limit ; 
@@ -21,7 +26,8 @@ export const get_category_form_controller = async (req ,res)=>{
 } 
 
 export const post_new_category_controller = async (req,res)=>{
-
+           const cateogry_name  = req.body.category_name ; 
+           await pool.query(``)
 }
 
 export const get_category_edit_form_controller = async (req,res)=>{
