6285db4eb413071ce2e1a20700cf99241649983b
faateh029 <buttfatih691@gmail.com>
2025-08-01 14:30:44 +0500

    Implement Category Model Scheema

---
+++ models/category.js
@@ -0,0 +1,21 @@
+//import { DataTypes, Sequelize } from "sequelize";
+
+export default (Sequelize , DataTypes)=>{
+    const Category = Sequelize.define("categories" , {
+        category_id:{
+            type:DataTypes.INTEGER , 
+            primaryKey:true ,
+            autoIncrement:true
+        },
+        category_name:{
+            type: DataTypes.TEXT ,
+            allowNull:false
+        }
+    } , {
+        timeStamps:false , 
+        freezeTableName:true
+     })
+     return Category;
+
+  
+}
