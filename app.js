1af65e5dd94c34080b4bf15d42bc63a79a7a7344
faateh029 <buttfatih691@gmail.com>
2025-08-02 23:39:30 +0500

    Import models and sequelize to entry point

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
-//import { sequelize, Category, Item } from './models/index.js';
 import ejs from 'ejs'
+import sequelize from './db.js';
+import { Item } from '../models/item.js';
+import { Category } from '../models/category.js';
 import methodOverride from 'method-override';
 import { inventoryRouter } from './routes/route.js';
 import { fileURLToPath } from 'url';
@@ -10,6 +12,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set('view engine' , 'ejs');
@@ -17,6 +21,15 @@ app.set('views' , path.join(__dirname , 'views'));
 app.use(methodOverride('_method'));
 
 app.use('/' , inventoryRouter);
+try {
+    await sequelize.authenticate();
+    console.log("db connected")
+    // db connected
+    await sequelize.sync({alter:true});
+    console.log("models synced");
+} catch (error) {
+    console.log("sequelize sync error" , error)
+}
 app.listen(PORT , (req,res)=>{
     console.log(`server running on port ${PORT}`)
 })
